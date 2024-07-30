538a660ae5086ea483a9284754b614fd7e96a36d
abhi <ugabhi@proton.me>
2024-07-30 14:40:35 +0530

    refactoring

--- home/abhi/pkgs.nix
+++ home/abhi/pkgs.nix
@@ -3,87 +3,86 @@
   home.packages = with pkgs; [
 
     alacritty
-    # cmake
-    # ripgrep
-    # playerctl
-    # neofetch
-    # xclip
-    # gcc
-    # pkg-config
-    # gnumake
-    # gnupatch
-    # git
-    # feh
-    # fzf
-    # vlc
-    # dconf
+   cmake
+   ripgrep
+   playerctl
+   neofetch
+   xclip
+   gcc
+   pkg-config
+   gnumake
+   gnupatch
+   git
+   feh
+   fzf
+   vlc
+   dconf
     rofi
     networkmanager
-    # unzip
-    # hashcat
-    # hashcat-utils
-    # john
-    # nmap
-    # font-manager
-    # tree-sitter-grammars.tree-sitter-latex
-    # kdePackages.okular
-    # fd
-    # ffmpeg
-    # pulsemixer
-    # #lxappearance
-    # tree-sitter
-    # #virtualbox
-    # calibre
-    # cinnamon.nemo-with-extensions
-    # texlive.combined.scheme-full
-    # zathura
-    # tree
+     unzip
+     hashcat
+     hashcat-utils
+     john
+     nmap
+     font-manager
+     tree-sitter-grammars.tree-sitter-latex
+     kdePackages.okular
+     fd
+     ffmpeg
+     pulsemixer
+     #lxappearance
+     tree-sitter
+     #virtualbox
+     calibre
+     cinnamon.nemo-with-extensions
+     texlive.combined.scheme-full
+     zathura
+     tree
     # betterlockscreen
-    # vim
-    # btop
-    # htop
-    # cava
-    # xsel # for clipboard
-    # mpc-cli
-    # rofimoji
-    # rofi-calc
-    # openssl_3_3
+     btop
+     htop
+     cava
+     xsel # for clipboard
+     mpc-cli
+     rofimoji
+     rofi-calc
+     openssl_3_3
+    
+     xorg.libxcb
+     xorg.libXrender
+     xorg.libXfixes
+     xorg.libXext
+     xorg.libX11
+     xorg.libXft
+     xorg.libXinerama
+    
     #
-    # xorg.libxcb
-    # xorg.libXrender
-    # xorg.libXfixes
-    # xorg.libXext
-    # xorg.libX11
-    # xorg.libXft
-    # xorg.libXinerama
-    #
-    #
-    # unstable.neovim
+    unstable.neovim
     # unstable.libreoffice-fresh
-    # unstable.librewolf
-    # unstable.youtube-music
-    # unstable.yazi
-    # unstable.telegram-desktop
+     unstable.librewolf
+     unstable.youtube-music
+     unstable.yazi
+     unstable.telegram-desktop
     # unstable.krita
     # unstable.inkscape-with-extensions
     # unstable.gimp
-    # unstable.binutils_nogold
-    # unstable.rust-analyzer
-    # unstable.python3
-    # unstable.lua-language-server
-    # unstable.nil
-    # unstable.ltex-ls
-    # unstable.lua
-    # unstable.luajitPackages.luarocks
-    # unstable.luajitPackages.jsregexp
-    # unstable.nodePackages_latest.nodejs
-    # unstable.nodejs_22
-    # unstable.pyright
-    # unstable.rustc
-    # unstable.rustfmt
-    # unstable.cargo
-    # unstable.clippy
-    # unstable.libudev-zero
+     unstable.binutils_nogold
+     unstable.rust-analyzer
+     unstable.python3
+     unstable.lua-language-server
+     unstable.nil
+     unstable.ltex-ls
+     unstable.lua
+     unstable.luajitPackages.luarocks
+     unstable.luajitPackages.jsregexp
+     unstable.nodePackages_latest.nodejs
+     unstable.nodejs_22
+     unstable.pyright
+     unstable.rustc
+     unstable.rustfmt
+     unstable.cargo
+     unstable.clippy
+     unstable.libudev-zero
 
     # (ncmpcpp.override { visualizerSupport = true; })
   ];
