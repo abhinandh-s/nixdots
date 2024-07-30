81cdc5f6beabfd35068d153910009ad0fb803cfb
abhi <ugabhi@proton.me>
2024-07-30 14:07:27 +0530

    refactoring

--- home/abhi/pkgs.nix
+++ home/abhi/pkgs.nix
@@ -2,29 +2,89 @@
 {
   home.packages = with pkgs; [
 
-    #(import ../abhi/mods/spotify-adblock/default.nix { inherit pkgs; })
-    #btop
-    #htop
+    alacritty
+    cmake
+    ripgrep
+    playerctl
+    neofetch
+    xclip
+    gcc
+    pkg-config
+    gnumake
+    gnupatch
+    git
+    feh
+    fzf
+    vlc
+    dconf
+    rofi
+    networkmanager
+    unzip
+    hashcat
+    hashcat-utils
+    john
+    nmap
+    font-manager
+    tree-sitter-grammars.tree-sitter-latex
+    kdePackages.okular
+    fd
+    ffmpeg
+    pulsemixer
+    #lxappearance
+    tree-sitter
+    #virtualbox
+    calibre
+    cinnamon.nemo-with-extensions
+    texlive.combined.scheme-full
+    zathura
+    tree
+    betterlockscreen
+    vim
+    btop
+    htop
     cava
     xsel # for clipboard
     mpc-cli
     rofimoji
-    #rofi-mpd
-    #picard
-    #gtk-engine-murrine # picard depentancy
     rofi-calc
     openssl_3_3
-    unstable.rust-analyzer
-    unstable.python3
-    unstable.lua-language-server
-    unstable.nil
+
     xorg.libxcb
     xorg.libXrender
     xorg.libXfixes
     xorg.libXext
+    xorg.libX11
+    xorg.libXft
+    xorg.libXinerama
+
+
+    unstable.neovim
+    unstable.libreoffice-fresh
+    unstable.librewolf
+    unstable.youtube-music
+    unstable.yazi
+    unstable.telegram-desktop
+    unstable.krita
+    unstable.inkscape-with-extensions
+    unstable.gimp
     unstable.binutils_nogold
-    unstable.pyright
+    unstable.rust-analyzer
+    unstable.python3
+    unstable.lua-language-server
+    unstable.nil
     unstable.ltex-ls
+    unstable.lua
+    unstable.luajitPackages.luarocks
+    unstable.luajitPackages.jsregexp
+    unstable.nodePackages_latest.nodejs
+    unstable.nodejs_22
+    unstable.pyright
+    unstable.rustc
+    unstable.rustfmt
+    unstable.cargo
+    unstable.clippy
+    unstable.libudev-zero
+
     # (ncmpcpp.override { visualizerSupport = true; })
   ];
 }
