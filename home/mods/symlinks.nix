dd44d1dced2ef2b507624ed79811008bab8160f3
Abhinandh S <ugabhi@proton.me>
2024-11-26 08:57:35 +0530

    calibre theme added

--- home/mods/symlinks.nix
+++ home/mods/symlinks.nix
@@ -1,4 +1,4 @@
-{ config, ... }
+{ pkgs, ... }
 :
 let
   kdeconnect_config = ''[General]
@@ -11,5 +11,16 @@ in
     ".local/bin/autostart.sh".source = ../../custom/bin/autostart.sh;
     ".ssh/id_ed25519.pub".source = ../../secrets/id_ed25519.pub;
     ".config/kdeconnect/config".text = kdeconnect_config;
+  };
+
+  xdg.configFile = {
+    "calibre/themes/catppuccin/mocha/red.calibre-palette".source = let
+      repo = pkgs.fetchFromGitHub {
+        "owner" = "catppuccin";
+        "repo" = "calibre";
+        "rev" = "27bd12d2d8dcd24b9ce7cae32135fbd41b896921";
+        "hash" = "sha256-JnovYvkIjQXHMFWsmAYIKiA1aexDI3+IkuwwpRdMIcs=";
+      };
+    in "${repo}/themes/mocha/catppuccin-mocha-red.calibre-palette";
   };
 }
